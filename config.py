fc82b330c367ca4577cf2f052b232eface67d9ac
hariDasu <harirao3@gmail.com>
2014-12-14 22:30:10 +0000

    first commit

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "A7XlhgAUCYa4TAVNkGZCNFrFa"
+consumer_secret = "zKDTmUV3mrgy96CcIeGyDqxA4e7LniNgbIFFXMsZM9XGyxzKhe"
+access_token = "22082713-EkSh3lYwV4Gs0Lc1luQUoQ1IBSrQxSIxCn7gyfyqE"
+access_token_secret = "14rx7wDxoprk4cvTIOX9KrI0advwpZ3IjjmWZYnFLbaQf"
